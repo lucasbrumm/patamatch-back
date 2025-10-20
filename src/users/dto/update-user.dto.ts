46a58f9f90021aee36583325fbc165fa080aa92d
lucas.lugao <lucasbrumfurtado@hotmail.com>
2025-10-20 17:41:19 -0300

    refactor: :recycle: rename findById to findOne in UsersController and simplify UpdateUserDto by removing OmitType

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,4 @@
-import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-// Excluindo explicitamente o campo id do DTO de atualização
-export class UpdateUserDto extends PartialType(
-  OmitType(CreateUserDto, ['id'] as const),
-) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
